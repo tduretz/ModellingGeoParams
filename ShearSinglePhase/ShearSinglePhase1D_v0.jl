cf9c77e81f8a58f37096df66a41e676caf6e674c
tduretz <duretz@uni-frankfurt.de>
2023-10-26 10:34:58 +0200

    Fixed dilation

--- ShearSinglePhase/ShearSinglePhase1D_v0.jl
+++ ShearSinglePhase/ShearSinglePhase1D_v0.jl
@@ -41,12 +41,12 @@ function main()
     σ          = Ly/40
     ε0         = nondimensionalize(1e-9s^-1, CharDim)
     G          = E/2.0/(1+ν)
-    Kb         = E/3.0/(1-2ν)
+    Kbulk      = E/3.0/(1-2ν)
     Coh0       = nondimensionalize(0Pa, CharDim)*40
     μs         = nondimensionalize(1e22Pa*s, CharDim)
     ϕ          = 43*π/180.
     ψ          = 15.0*π/180.   
-    ηvp        = nondimensionalize(2*1e11Pa*s, CharDim)     
+    ηvp        = nondimensionalize(1*1e11Pa*s, CharDim)     
 
     # Numerical parameters
     Ncy        = 100
@@ -58,41 +58,54 @@ function main()
     ηe         = G*Δt
 
     # Allocate arrays
-    Pt         = Pi*ones(Ncy+1) 
-    Ptc        = Pi*ones(Ncy+1) 
-    Pt0        = Pi*ones(Ncy+1)
-    τxx        = τxxi*ones(Ncy+1)
-    τxy        = τxyi*ones(Ncy+1)
-    τyy        = τyyi*ones(Ncy+1)  #.+ Δτ.*exp.(-yv.^2/2σ^2)  
-    τzz        = τzzi*ones(Ncy+1)  #.+ Δτ.*exp.(-yv.^2/2σ^2)  
-    τxx0       = τxxi*ones(Ncy+1)
-    τxy0       = τxyi*ones(Ncy+1)
-    τyy0       = τyyi*ones(Ncy+1)  #.+ Δτ.*exp.(-yv.^2/2σ^2) 
-    τzz0       = τzzi*ones(Ncy+1)  #.+ Δτ.*exp.(-yv.^2/2σ^2) 
-    Coh        = zeros((Ncy+1)); Coh .= Coh0 .- Coh0./10 .* exp.(-yv.^2/2σ^2)
-    F          = zeros((Ncy+1))
-    Fc         = zeros((Ncy+1))
-    λ̇          = zeros((Ncy+1))
-    λ̇rel       = zeros((Ncy+1))
-    ispl       = zeros(Int, (Ncy+1))
-    ηve        = zeros((Ncy+1)); ηve        .= 1.0./(1.0/μs .+ 1.0/ηe)
-    ηvep       = zeros((Ncy+1))
-    ε̇xy        = ε0*ones(Ncy+1)
-    ε̇yy        =   zeros(Ncy+1)
-    ε̇iiᵉᶠᶠ     =   zeros(Ncy+1)
-    τii        =   zeros(Ncy+1)
-    η          =   zeros(Ncy+1)
-    ΔτV        =   zeros(Ncy)
-    ΔτPt       =   zeros(Ncy+1)
-    η_mm       =   zeros(Ncy)
-    Vx         =   zeros(Ncy+2);  Vx .= ε0.*yc
-    Vy         =   zeros(Ncy+2);
-    RPt        =   zeros(Ncy+1)
-    RVx        =   zeros(Ncy+2)
-    RVy        =   zeros(Ncy+2)
-    ∂Pt∂τ      =   zeros(Ncy+1)
-    ∂Vx∂τ      =   zeros(Ncy+2)
-    ∂Vy∂τ      =   zeros(Ncy+2)
+    Pt         =  Pi*ones(Ncy+1) 
+    Ptc        =  Pi*ones(Ncy+1) 
+    Pt0        =  Pi*ones(Ncy+1)
+    τxx        =  τxxi*ones(Ncy+1)
+    τxy        =  τxyi*ones(Ncy+1)
+    τyy        =  τyyi*ones(Ncy+1)    
+    τzz        =  τzzi*ones(Ncy+1)  
+    τxx0       =  τxxi*ones(Ncy+1)
+    τxy0       =  τxyi*ones(Ncy+1)
+    τyy0       =  τyyi*ones(Ncy+1)
+    τzz0       =  τzzi*ones(Ncy+1)
+    Kb         = Kbulk*ones(Ncy+1); Kb[50] = 2 *Kbulk #.- Coh0 .* exp.(-yv.^2/2σ^2)
+    Coh        =  zeros((Ncy+1));    
+    F          =  zeros((Ncy+1))
+    Fc         =  zeros((Ncy+1))
+    λ̇          =  zeros((Ncy+1))
+    λ̇rel       =  zeros((Ncy+1))
+    ispl       =  zeros(Int, (Ncy+1))
+    ηve        =  zeros((Ncy+1)); ηve        .= 1.0./(1.0/μs .+ 1.0/ηe)
+    ηvep       =  zeros((Ncy+1))
+    ε̇xy        =  ε0*ones(Ncy+1)
+    ε̇yy        =    zeros(Ncy+1)
+    ε̇iiᵉᶠᶠ     =    zeros(Ncy+1)
+    τii        =    zeros(Ncy+1)
+    η          =    zeros(Ncy+1)
+    ΔτV        =    zeros(Ncy)
+    ΔτPt       =    zeros(Ncy+1)
+    η_mm       =    zeros(Ncy)
+    Vx         =    zeros(Ncy+2);  Vx .= ε0.*yc
+    Vy         =    zeros(Ncy+2);
+    RPt        =    zeros(Ncy+1)
+    RVx        =    zeros(Ncy+2)
+    RVy        =    zeros(Ncy+2)
+    ∂Pt∂τ      =    zeros(Ncy+1)
+    ∂Vx∂τ      =    zeros(Ncy+2)
+    ∂Vy∂τ      =    zeros(Ncy+2)
+ 
+    ε̇xy_pl     =   zeros(Ncy+1)
+    ε̇yy_pl     =   zeros(Ncy+1)
+    ε̇xy_el     =   zeros(Ncy+1)
+    ε̇yy_el     =   zeros(Ncy+1)
+    ε̇xy_net    =   zeros(Ncy+1)
+    ε̇yy_net    =   zeros(Ncy+1)
+
+    ∇v         =   zeros(Ncy+1)
+    ∇v_el      =   zeros(Ncy+1)
+    ∇v_pl      =   zeros(Ncy+1)
+    ∇v_net     =   zeros(Ncy+1)
 
     # Monitoring
     probes    = (Ẇ0 = zeros(Nt), τxy0 = zeros(Nt), σyy0 = zeros(Nt), Vx0  = zeros(Nt), τii)
@@ -134,7 +147,7 @@ function main()
             if BC_Vy == :Dirichlet
                 Vy[end] = - Vy[end-1] + 2VyN
             elseif BC_Vy == :Neumann   
-                Vy[end] =  Vy[end-1] + Δy/(4//3*ηve[end] + Kb*Δt)*(σyyi + Pt0[end] - τyy0[end]/ηe*ηve[end])
+                Vy[end] =  Vy[end-1] + Δy/(4//3*ηve[end] + Kb[end]*Δt)*(σyyi + Pt0[end] - τyy0[end]/ηe*ηve[end])
             end
             @. ε̇xy     =  0.5*(Vx[2:end] - Vx[1:end-1])/Δy
             @. ε̇yy     = 2//3*(Vy[2:end] - Vy[1:end-1])/Δy # deviatoric
@@ -146,7 +159,7 @@ function main()
             @. τzz     =  2 * ηve * (0.0 + τzz0/2/ηe)
             @. τii     = sqrt(τxy^2 + 0.5*(τyy.^2 + τxx.^2 + τzz.^2))
 
-            # Plasticity
+            # # Plasticity
             @. F    = τii - Coh*cos(ϕ) - Pt*sin(ϕ)
             @. Ptc  = Pt
             @. ηvep = ηve
@@ -155,9 +168,9 @@ function main()
                 ε̇iiᵉᶠᶠ[pl]   = sqrt( (ε̇xy[pl] + τxy0[pl]/2/ηe)^2 + 0.5*( (0.0 + τxx0[pl]/2/ηe)^2 + ((ε̇yy[pl] + τyy0[pl]/2/ηe)).^2 + ((0.0 + τzz0[pl]/2/ηe)).^2 ) ) 
                 if F[pl] > 0.
                     ispl[pl] = 1
-                    λ̇[pl]    = F[pl] / (ηvp + ηve[pl] + Kb*Δt*sin(ϕ)*sin(ψ))
+                    λ̇[pl]    = F[pl] / (ηvp + ηve[pl] + Kb[pl]*Δt*sin(ϕ)*sin(ψ))
                     λ̇rel[pl] = (1.0-rel)*λ̇rel[pl] + rel*λ̇[pl]   
-                    Ptc[pl]  = Pt[pl] + Kb*Δt*sin(ψ)*λ̇rel[pl]
+                    Ptc[pl]  = Pt[pl] + Kb[pl]*Δt*sin(ψ)*λ̇rel[pl]
                     ηvep[pl] = (Coh[pl]*cos(ϕ) + Ptc[pl]*sin(ϕ) + ηvp*λ̇rel[pl]) / 2.0 / ε̇iiᵉᶠᶠ[pl]
                     τxy[pl]  =  2 * ηve[pl] * (ε̇xy[pl] + τxy0[pl]/2/ηe - τxy[pl]/τii[pl]/2*λ̇rel[pl] ) 
                     τyy[pl]  =  2 * ηve[pl] * (ε̇yy[pl] + τyy0[pl]/2/ηe - τyy[pl]/τii[pl]/2*λ̇rel[pl] )
@@ -168,13 +181,25 @@ function main()
                 end
             end
 
+            # Check
+            @. ∇v      = (Vy[2:end] - Vy[1:end-1])/Δy
+            @. ε̇yy_el  =  (τyy - τyy0)/2/ηe
+            @. ε̇yy_pl  =   τyy/τii/2*λ̇rel
+            @. ε̇xy_el  =  (τxy - τxy0)/(2*ηe)
+            @. ε̇xy_pl  =   τxy/τii/2*λ̇rel
+            @. ∇v_el   =  -(Ptc - Pt0)/Kb/Δt
+            @. ∇v_pl   =  λ̇rel*sin(ψ)
+            @. ε̇xy_net = ε̇xy - ε̇xy_el - ε̇xy_pl
+            @. ε̇yy_net = ε̇yy - ε̇yy_el - ε̇yy_pl
+            @. ∇v_net  = ∇v  - ∇v_el  - ∇v_pl
+
             # PT time steps
             @. η_mm = min.(ηvep[1:end-1], ηvep[2:end]); 
             @. ΔτV   = Δy^2/(η_mm)/2.1 /4
             @. ΔτPt  = ηvep/Δy/Kb/Δt/50
             
             # Residuals
-            @. RPt = - Kb*Δt*(Vy[2:end]  - Vy[1:end-1] )/Δy - (Pt - Pt0)
+            @. RPt          = - Kb*Δt*(Vy[2:end]  - Vy[1:end-1] )/Δy - (Pt - Pt0)
             @. RVx[2:end-1] =   (τxy[2:end] - τxy[1:end-1])/Δy 
             @. RVy[2:end-1] =   (τyy[2:end] - τyy[1:end-1])/Δy - (Ptc[2:end] - Ptc[1:end-1])/Δy
             
@@ -193,6 +218,8 @@ function main()
                 errVx = norm(RVx)/sqrt(length(RVx))
                 errVy = norm(RVy)/sqrt(length(RVy))
                 @printf("Iteration %05d --- Time step %4d --- Δt = %2.2e --- ΔtC = %2.2e --- εxy = %2.2e --- max(F) = %2.2e --- max(Fc) = %2.2e \n", iter, it, ustrip(dimensionalize(Δt, s, CharDim)), ustrip(dimensionalize(Δy/2/maximum(Vx), s, CharDim)), ε0*it*Δt, maximum(F), maximum(Fc))
+                @printf("Exy_net = %2.2e --- Eyy_net = %2.2e --- Div net = %2.2e\n", mean(abs.(ε̇xy_net)), mean(abs.(ε̇yy_net)), mean(abs.(∇v_net)) )
+                @printf("Exy_el  = %2.2e --- Exy_pl  = %2.2e --- Exy net = %2.2e\n", mean(abs.(ε̇xy_el)), mean(abs.(ε̇xy_pl)), mean(abs.(ε̇xy_net)) )
                 @printf("fPt = %2.4e\n", errPt)
                 @printf("fVx = %2.4e\n", errVx)
                 @printf("fVy = %2.4e\n", errVy)
@@ -200,6 +227,9 @@ function main()
                 (isnan(errPt) || isnan(errVx) || isnan(errVx)) && error("NaNs")        
             end
         end
+
+        @. Pt = Ptc
+
         if (errPt > ϵ || errVx > ϵ || errVx > ϵ) error("non converged") end
 
         probes.Ẇ0[it]         = τxy[end]*ε̇xy[end]
@@ -219,10 +249,10 @@ function main()
             p2=plot(title = "Velocity", xlabel = L"$Vx$ [cm/y]", ylabel = L"$y$ [km]" )
             p2=plot!(ustrip.(dimensionalize(Vx, m/s, CharDim))*cmy, ustrip.(dimensionalize(yc, m, CharDim)./1e3) )
 
-            p3=plot(title = "σ1 angle", xlabel = L"$angle$ [kPa]", ylabel = L"$y$ [km]" )
+            p3=plot(title = "σ1 angle", xlabel = "angle", ylabel = L"$y$ [km]" )
             p3=plot!(ustrip.(-atand.(σ1.z[:] ./ σ1.x[:])), ustrip.(dimensionalize(yv, m, CharDim)./1e3), xlimits=(10,60) )
 
-            p4=plot(title = "Probes", xlabel = "Angle", ylabel = L"[-]" )
+            p4=plot(title = "Probes", xlabel = "Strain", ylabel = L"[-]" )
             # p4=plot!(1:it, ustrip.(dimensionalize(probes.τxy0[1:it], Pa, CharDim))/1e3, label="τxy" )
             p4=plot!((1:it)*ε0*Δt, ustrip.(-probes.τxy0[1:it]./probes.σyy0[1:it]), label="-τxy/σyyBC" )
             # p4=plot!(1:it, ustrip.(dimensionalize(-probes.σyy0[1:it], Pa, CharDim))/1e3, label="σyyBC" )
@@ -233,7 +263,7 @@ function main()
     end
 end
 
-main()
+@show main()
 
 # function getellipsepoints(cx, cy, rx, ry, θ)
 # 	t = range(0, 2*pi, length=100)
